67fe3ffb4a203ed1eb716edb2b179479ac12575c
NarDd <1601037@SIT.Singaporetech.edu.sg>
2018-05-21 08:33:40 +0800

    Created test function that extracts all text values

--- lottery.py
+++ lottery.py
@@ -1,5 +1,6 @@
 import requests
 import os.path
+import pandas as pd
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -8,22 +9,66 @@ from datetime import datetime
 # URLS
 url = "http://www.singaporepools.com.sg/en/product/Pages/4d_results.aspx"
 
-# Start Selenium browser get page
-browser = webdriver.Chrome()
-browser.get(url)
-soup = BeautifulSoup(browser.page_source, "html.parser")
+# Functions
+def getPage():
+    # Start Selenium browser get page
+    browser = webdriver.Chrome()
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
 
-# Find Filepath
-mainPath = os.path.dirname(os.path.realpath(__file__))
-resultsPath = "output"
-i = datetime.now()
-print(i.strftime('%Y%m%d%H%M%S'))
-filename = i.strftime('%Y%m%d%H%M%S') + ".txt"
-filepath = os.path.join(mainPath, resultsPath, filename)
+    # Find Filepath
+    mainPath = os.path.dirname(os.path.realpath(__file__))
+    resultsPath = "output"
+    i = datetime.now()
+    print(i.strftime('%Y%m%d%H%M%S'))
+    filename = i.strftime('%Y%m%d%H%M%S') + ".txt"
+    filepath = os.path.join(mainPath, resultsPath, filename)
 
-# Write result to file
-try:
-    with open(filepath, "w",encoding="utf-8") as file:
-        file.write(str(soup))
-except IOError:
-    print("Unable to write to file")
+    # Write result to file
+    try:
+        with open(filepath, "w",encoding="utf-8") as file:
+            file.write(str(soup))
+    except IOError:
+        print("Unable to write to file")
+    return
+
+def Test():
+    html_report = open('20180521064435.html','r',encoding="utf-8").read()
+    soup = BeautifulSoup(html_report, "html.parser")
+
+    #Top Results Table Processing
+    topTable = soup.find("table",{"class":"table table-striped orange-header"})
+    drawDate = topTable.find("th",{"class":"drawDate"}).get_text()
+    drawNumber = topTable.find("th",{"class":"drawNumber"}).get_text()
+    print(drawDate)
+    print(drawNumber)
+    for row in topTable.findAll('tr'):
+        tableDatas = row.findAll('td')
+        for tableData in tableDatas:
+            print(tableData.get_text())
+
+    #Starter Results Table Processing
+    starterTable = soup.find("tbody",{"class":"tbodyStarterPrizes"})
+    for row in starterTable.findAll('tr'):
+        columns = row.findAll('td')
+        for column in columns:
+            print(column.get_text())
+
+    #Consolation Results table Processing
+    consolTable = soup.find("tbody",{"class":"tbodyConsolationPrizes"})
+    for row in consolTable.findAll('tr'):
+        columns = row.findAll('td')
+        for column in columns:
+            print(column.get_text())
+
+    allTable = str(topTable) + str(starterTable) + str(consolTable)
+    try:
+        with open("testoutput.txt", "w",encoding="utf-8") as file:
+            file.write(str(allTable))
+    except IOError:
+        print("Unable to write to file")
+    return
+
+# Main
+# getPage();
+Test();
